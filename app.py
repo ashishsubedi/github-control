cd5c60d6d2211ef0ec75a518601b2c0feb3cf0ee
Ashish Subedi <ashishsubedi10@outlook.com>
2020-04-14 20:30:47 +0545

    Added functionality to create repo and automatically initialize setup

--- app.py
+++ app.py
@@ -1,41 +1,50 @@
 from config import ACCESS_TOKEN
 from github import Github
+import pygit2
 import os
-import sys,argparse
+import sys
+import argparse
+import base64
 
 g = Github(ACCESS_TOKEN)
 
+
 def main():
     global g
-    try:
-        parser = argparse.ArgumentParser(description="Automatically Creates github repo in the path")
-        parser.add_argument('-n','--name',help='Name of the repository',required=True)
-        parser.add_argument('-d','--description',help='Description of the repository')
-        parser.add_argument('-s','--private',help='Add this to make repo private',action='store_true')
-        parser.add_argument('-p','--path',help="Root Path of the working folder",required=True)
-        args = parser.parse_args()
+    commit_msg = "Initial Commit"
 
-        name,path,desc,private = args.name,args.path,args.description,args.private
-        
-        user = g.get_user()
-        repo = user.create_repo(name,desc,private=private)
-        if not os.path.exists(path):
-            os.mkdir(path)
-            with open(os.path.join(path,'Readme.md'),'w') as f:
-                f.write("Created automatically using create-github")
-        
-        
+    parser = argparse.ArgumentParser(
+        description="Automatically Creates github repo in the path")
+    parser.add_argument(
+        '-n', '--name', help='Name of the repository', required=True)
+    parser.add_argument('-d', '--description',
+                        help='Description of the repository')
+    parser.add_argument(
+        '-s', '--private', help='Add this to make repo private', action='store_true')
+    parser.add_argument(
+        '-p', '--path', help="Root Path of the working folder", required=True)
+    args = parser.parse_args()
 
-        
-        
+    name, path, desc, private = args.name, args.path, args.description, args.private
+    if desc == None:
+        desc = ''
+    user = g.get_user()
 
-        
+    repo = user.create_repo(name, desc, private=private)
 
+    content_msg = "Created automatically using create-github"
 
+    repo.create_file("Readme.md", commit_msg, content_msg)
+    
+    credentials = pygit2.UserPass(user.email, ACCESS_TOKEN)
+    callbacks=pygit2.RemoteCallbacks(credentials=credentials)
+    repoClone = pygit2.clone_repository(repo.clone_url, path,callbacks=callbacks)
+    print(repo.git_url,repo.clone_url)
 
+    repoClone.remotes.set_push_url("origin",repo.clone_url)
 
-    except Exception as e:
-        print(e)
+    print('[SUCCESSFULLY CREATED REPO]')
+    print(repo.clone_url)
     
 if __name__ == '__main__':
     main()
