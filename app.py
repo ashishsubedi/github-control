6d2947d5191467dc50b57f7c9246586244996410
Ashish Subedi <ashishsubedi10@outlook.com>
2020-04-14 17:18:45 +0545

    Added arguments parser and getting user info

--- app.py
+++ app.py
@@ -1,8 +1,41 @@
+from config import ACCESS_TOKEN
 from github import Github
+import os
+import sys,argparse
 
-# First create a Github instance:
+g = Github(ACCESS_TOKEN)
+
+def main():
+    global g
+    try:
+        parser = argparse.ArgumentParser(description="Automatically Creates github repo in the path")
+        parser.add_argument('-n','--name',help='Name of the repository',required=True)
+        parser.add_argument('-d','--description',help='Description of the repository')
+        parser.add_argument('-s','--private',help='Add this to make repo private',action='store_true')
+        parser.add_argument('-p','--path',help="Root Path of the working folder",required=True)
+        args = parser.parse_args()
+
+        name,path,desc,private = args.name,args.path,args.description,args.private
+        
+        user = g.get_user()
+        repo = user.create_repo(name,desc,private=private)
+        if not os.path.exists(path):
+            os.mkdir(path)
+            with open(os.path.join(path,'Readme.md'),'w') as f:
+                f.write("Created automatically using create-github")
+        
+        
+
+        
+        
+
+        
 
 
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
+
+
+    except Exception as e:
+        print(e)
+    
+if __name__ == '__main__':
+    main()
